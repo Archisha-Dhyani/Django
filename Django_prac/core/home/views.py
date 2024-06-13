261e07cc2db9173cd4ee79cfbcedda06a83e567c
Archisha-Dhyani <archishadhyani8@gmail.com>
2024-06-13 23:39:00 +0530

    Day 04

--- Django_prac/core/home/views.py
+++ Django_prac/core/home/views.py
@@ -10,7 +10,16 @@ def home(request):
         {'name': 'Khushi ','age': 19},
         {'name': 'Mysha ','age': 7}
     ]
+    vegetables=['Pumpkin','Tomamto','Potato']
 
 
-    return render(request,"index.html",context = {"people":people})
+    return render(request,"index.html",context = {"people":people,"vegetables":vegetables})
+
+def about(request):
+
+    return render(request,"about.html",)
+
+def contact(request):
+
+    return render(request,"contact.html",)
 
